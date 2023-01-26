656b7aced1034debe7309f393b0e3877b736c754
Abner Suhett <suhettabner@gmail.com>
2023-01-26 10:14:43 -0300

    atividade 6

--- Programação web/semana 6/atividade pratica 06/index.js
+++ Programação web/semana 6/atividade pratica 06/index.js
@@ -1,5 +1,5 @@
 /*
-Faça um algoritmo que leia a idade de vários alunos de uma turma.
+1- Faça um algoritmo que leia a idade de vários alunos de uma turma.
 O programa vai parar quando for digitada a idade 999. No final,
 mostre quantos alunos existem na turma e qual é a média de idade
 do grupo.
