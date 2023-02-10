80404f1861dd0f6233fbff656a97100844c91a0c
Abner Suhett <suhettabner@gmail.com>
2023-02-09 21:20:00 -0300

    organizando pasta

--- Programação web/semana 8/Atividade pratica  11 - objetos/script.js
+++ Programação web/semana 8/Atividade pratica  11 - objetos/script.js
@@ -138,3 +138,27 @@ function ex4() {
   }
   filtraSkill(alunos, "Javascript");
 }
+
+/*
+5. Crie um cadastro de pessoas onde o usuário informe o nome, idade
+e se está trabalhando ou não, se a pessoa estiver trabalhando
+pergunte para ele o salário que está ganhando. Para cada pessoa
+cadastrada, pergunte ao usuário se ele deseja continuar
+cadastrando ou não. No final, mostre as pessoas que estão
+desempregadas, as pessoas que estão empregadas separadas
+pelas que ganham mais que 2500 e menos que 2500.
+
+Exemplo de resultado:
+Pessoas desempregadas:
+Nome: Alessandro, Idade: 28
+Nome: Alessandro, Idade: 28
+
+Pessoas empregadas com salários menores que 2500:
+Nome: Alessandro, Idade: 28, Salário: 1500
+Nome: Alessandro, Idade: 28, Salário: 2400
+
+Pessoas empregadas com salários maiores que 2500:
+Nome: Alessandro, Idade: 28, Salário: 2700
+Nome: Alessandro, Idade: 28, Salário: 3000
+*/
+
