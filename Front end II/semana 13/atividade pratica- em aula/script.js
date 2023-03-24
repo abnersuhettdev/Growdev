df95d2c619dec45953d6c3e810bcb567c254a1cb
Abner <abner.suhett@gmail.com>
2023-03-23 22:45:13 -0300

    atividade em aula

--- Front end II/semana 13/atividade pratica- em aula/script.js
+++ Front end II/semana 13/atividade pratica- em aula/script.js
@@ -1,10 +1,14 @@
 const form = document.querySelector("#form-cadastro");
 const divContatos = document.querySelector("#contatos");
+const containerNotification = document.querySelector('.toast-container');
 
 //como manipular um MODAL do Bootstrap utilizando JS
 const modalCadastro = new bootstrap.Modal("#modal-cadastro");
+const modalApagar = new bootstrap.Modal("#modal-apagar");
 
 let listaContatos = JSON.parse(localStorage.getItem("contatos")) ?? [];
+
+
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -22,7 +26,8 @@ form.addEventListener("submit", (e) => {
   const existe = listaContatos.some((contato) => contato.phone === phone);
 
   if (existe) {
-    alert("Você já cadastrou esse contato anteriormente!");
+    modalCadastro.hide()
+    showAlert('danger','Você já cadastrou esse contato');
     return;
   }
 
@@ -37,6 +42,7 @@ form.addEventListener("submit", (e) => {
   form.reset();
   addContato(contato);
   modalCadastro.hide();
+  form.classList.remove('was-validated')
 });
 
 function criaID() {
@@ -44,8 +50,11 @@ function criaID() {
 }
 
 function addContato(contato) {
+  const {id, name, phone} = contato
+
   const divCol = document.createElement("div");
   divCol.setAttribute("class", "col-12 col-sm-6 col-lg-4 col-xl-3");
+  divCol.setAttribute("id", `${id}`)
 
   const divCard = document.createElement("div");
   divCard.classList.add("card");
@@ -55,18 +64,25 @@ function addContato(contato) {
 
   const cardTitle = document.createElement("h5");
   cardTitle.classList.add("card-title");
-  cardTitle.innerText = contato.name;
+  cardTitle.innerText = name;
 
   const cardText = document.createElement("p");
   cardText.classList.add("card-text");
-  cardText.innerText = contato.phone;
+  cardText.innerText = phone;
 
   const editBtn = document.createElement("button");
   editBtn.classList.add("btn", "btn-success", "m-1");
+  editBtn.setAttribute('onclick', `editar("${id}", "${name}","${phone}")`)
   editBtn.innerHTML = '<i class="bi bi-pencil-square"></i>';
 
   const deleteBtn = document.createElement("button");
   deleteBtn.classList.add("btn", "btn-danger", "m-1");
+  deleteBtn.addEventListener('click', ()=>{
+    modalApagar.show();
+    const confirmarDelete = document.querySelector('#deletar');
+    confirmarDelete.setAttribute('onclick',`deletar(${id})`);
+  })
+
   deleteBtn.innerHTML = '<i class="bi bi-trash-fill "></i>';
 
   divCardBody.appendChild(cardTitle);
@@ -78,3 +94,63 @@ function addContato(contato) {
 
   divContatos.appendChild(divCol);
 }
+
+function editar(id, name, phone){
+  console.log(id)
+  console.log(name)
+  console.log(phone)
+}
+
+function deletar(id){
+  //excluir da lista de contatos local
+  const indiceId = listaContatos.findIndex(
+  (contato)=> contato.id === id)
+  listaContatos.splice(indiceId, 1)
+  console.log(listaContatos)
+
+  //atualizar localstorage
+  localStorage.setItem("contatos", JSON.stringify(listaContatos))
+
+  //excluir a col da dom
+  const col = document.getElementById(`${id}`)
+  col.remove()
+  
+
+  modalApagar.hide()
+}
+
+function showAlert(modo, mensagem){
+
+  const toast = document.createElement('div')
+  toast.setAttribute('role', 'alert');
+  toast.setAttribute('aria-live', "assertive")
+  toast.setAttribute('aria-atomic', 'true');
+  toast.setAttribute('class', 'toast align-items-center border-0 show')
+  toast.classList.add(`text-bg-${modo}`)
+
+  const content = document.createElement('div');
+  content.classList.add('d-flex')
+
+  const toastBody = document.createElement('div');
+  toastBody.classList.add('toast-body');
+  toastBody.innerText = mensagem
+
+  const btn = document.createElement('button')
+  btn.setAttribute('type', 'button')
+  btn.setAttribute('class', 'btn-close btn-close-white me-2 m-auto')
+  btn.setAttribute('data-bs-dismiss', 'toast')
+  btn.setAttribute('aria-label', 'Close')
+
+  content.appendChild(toastBody)
+  content.appendChild(btn)
+  toast.appendChild(content)
+
+  containerNotification.appendChild(toast)
+}
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  listaContatos.forEach(contato => {
+    addContato(contato)
+  });
+})
